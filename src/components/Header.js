a6bc065d1f95d521c7559847608b2453bd6d8b65
paras <paraskangra14@gmail.com>
2021-07-05 22:55:33 +0530

    made login component

---
+++ src/components/Header.js
@@ -0,0 +1,132 @@
+import React from "react";
+import styled from "styled-components";
+import AddIcon from "@material-ui/icons/Add";
+import { Button } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import StarIcon from "@material-ui/icons/Star";
+import TheatersIcon from "@material-ui/icons/Theaters";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+const Header = () => {
+  return (
+    <Container>
+      <DisneyIcon src="images/logo.svg" />
+      <NavMenu>
+        <IconComp>
+          <HomeIcon />
+          <p>Home</p>
+        </IconComp>
+        <IconComp>
+          <SearchIcon />
+          <p>Search</p>
+        </IconComp>
+        <IconComp>
+          <AddIcon />
+          <p>WatchList</p>
+        </IconComp>
+        <IconComp>
+          <StarIcon />
+          <p>Originals</p>
+        </IconComp>
+        <IconComp>
+          <TheatersIcon />
+          <p>Movies</p>
+        </IconComp>
+        <IconComp>
+          <LiveTvIcon />
+          <p>Series</p>
+        </IconComp>
+      </NavMenu>
+      <ButtonComp>
+        <Button variant="outlined">Login</Button>
+      </ButtonComp>
+    </Container>
+  );
+};
+
+const IconComp = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  margin-left: 20px;
+  & > p {
+    margin-left: 10px;
+    text-transform: uppercase;
+    letter-spacing: 1.4px;
+    font-weight: 400;
+    font-size: 13px;
+    color: rgb(249, 249, 249);
+    padding: 2px 0px;
+    white-space: nowrap;
+    position: relative;
+    /* border-bottom: 2px solid rgba(249, 249, 249); */
+    &:after {
+      content: " ";
+      position: absolute;
+      width: 100%;
+      transform: scaleX(0);
+      height: 2px;
+      bottom: -8px;
+      border-radius: 25px;
+      left: 0;
+      background-color: rgb(249, 249, 249);
+      transform-origin: bottom left;
+      opacity: 0;
+      transition: all 0.25s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    }
+    &:hover:after {
+      transform: scaleX(1);
+      transform-origin: bottom left;
+      opacity: 1;
+    }
+  }
+`;
+const DisneyIcon = styled.img`
+  object-fit: contain;
+  height: 50px;
+  margin-left: 10px;
+`;
+const ButtonComp = styled.div`
+  & > button {
+    color: #f9f9f9 !important;
+    border-color: #f9f9f9 !important;
+  }
+  & > button:hover {
+    opacity: 0.5;
+  }
+  padding-right: 5px;
+  margin-right: 10px;
+`;
+const Container = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 3;
+  background-color: #090b13;
+  padding: 0 36px;
+  height: 70px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  position: relative;
+  margin: 0;
+  padding: 0;
+  margin-right: auto;
+  padding-right: 30px;
+  margin-left: 15px;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+export default Header;
---
+++ src/components/Login.js
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components";
+const Login = () => {
+  return (
+    <Container>
+      <Content>
+        <BgImage />
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" />
+          <LoginButton>GET ALL THERE</LoginButton>
+          <LoginText>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </LoginText>
+          <CTALogoTwo src="/images/cta-logo-two.png"></CTALogoTwo>
+        </CTA>
+      </Content>
+    </Container>
+  );
+};
+
+const LoginButton = styled.a`
+  width: 100%;
+  max-width: 600px;
+  height: 50px;
+  margin-bottom: 12px;
+  word-spacing: 2px;
+  letter-spacing: 1.3px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  border: none;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 35px 0;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+const LoginText = styled.p`
+  width: 100%;
+  max-width: 580px;
+  font-size: 11px;
+  margin-bottom: 20px;
+  letter-spacing: 1.5px;
+  line-height: 1.5;
+`;
+const CTALogoTwo = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  width: 100%;
+  display: block;
+  object-fit: contain;
+`;
+const CTA = styled.div`
+  /* margin-bottom: ; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 650px;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+  margin: 0 auto;
+  transition-timing-function: ease-out;
+  transition: opacity 2s;
+  /* width: 100%; */
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  width: 100%;
+  display: block;
+  object-fit: contain;
+`;
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  flex-direction: column;
+  justify-content: center;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  background-image: url("/images/login-background.jpg");
+  height: 100%;
+
+  background-position: top;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  background-size: cover;
+`;
+
+export default Login;
